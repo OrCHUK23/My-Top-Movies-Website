d84705bdee2c8555fe3d5ee85316a65fa749bcb1
Or <henor1997@gmail.com>
2023-05-12 11:15:58 +0300

    Adding some more comments

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+"""
+Config file to initialize the db.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 
 # Configure the SQLite database, relative to the app instance folder.
@@ -5,10 +9,12 @@ DATABASE_URI = "sqlite:///movies-collection.db"
 
 db = SQLAlchemy()
 
+
 def init_app(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
 
 def create_app(app):
     # Create the db tables.
